9473bb1f2448435bfac6d10deffdc4e4134b558d
Maximilian Leith <maximilian@leith.de>
2023-10-21 16:49:46 +0200

    progres

--- src/appleTVEnhancedPlatform.ts
+++ src/appleTVEnhancedPlatform.ts
@@ -45,29 +45,17 @@ export class AppleTVEnhancedPlatform implements DynamicPlatformPlugin {
    * Accessories must only be registered once, previously created accessories
    * must not be registered again to prevent "duplicate UUID" errors.
    */
-    discoverDevices() {
+    async discoverDevices() {
 
-        // EXAMPLE ONLY
-        // A real plugin you would discover accessories from the local network, cloud services
-        // or a user-defined array in the platform config.
-        const exampleDevices = [
-            {
-                exampleUniqueId: 'ABCD',
-                exampleDisplayName: 'Bedroom',
-            },
-            {
-                exampleUniqueId: 'EFGH',
-                exampleDisplayName: 'Kitchen',
-            },
-        ];
+        const devices = (await pyatvInstance.find()).filter((d) => d.model?.startsWith('AppleTV'));
 
         // loop over the discovered devices and register each one if it has not already been registered
-        for (const device of exampleDevices) {
+        for (const device of devices) {
 
             // generate a unique id for the accessory this should be generated from
             // something globally unique, but constant, for example, the device serial
             // number or MAC address
-            const uuid = this.api.hap.uuid.generate(device.exampleUniqueId);
+            const uuid = this.api.hap.uuid.generate(device.id as string);
 
             // see if an accessory with the same uuid has already been registered and restored from
             // the cached devices we stored in the `configureAccessory` method above
@@ -91,21 +79,21 @@ export class AppleTVEnhancedPlatform implements DynamicPlatformPlugin {
                 // this.log.info('Removing existing accessory from cache:', existingAccessory.displayName);
             } else {
                 // the accessory does not yet exist, so we need to create it
-                this.log.info('Adding new accessory:', device.exampleDisplayName);
+                this.log.info('Adding new accessory:', device.name);
 
                 // create a new accessory
-                const accessory = new this.api.platformAccessory(device.exampleDisplayName, uuid);
+                const accessory = new this.api.platformAccessory(device.name, uuid);
 
                 // store a copy of the device object in the `accessory.context`
                 // the `context` property can be used to store any data about the accessory you may need
-                accessory.context.device = device;
+                accessory.context.id = device.id;
 
                 // create the accessory handler for the newly create accessory
                 // this is imported from `platformAccessory.ts`
                 new AppleTVEnhancedAccessory(this, accessory);
 
                 // link the accessory to your platform
-                this.api.registerPlatformAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
+                this.api.publishExternalAccessories(PLUGIN_NAME, PLATFORM_NAME, [accessory]);
             }
         }
     }
--- src/pyatvInstance.ts
+++ src/pyatvInstance.ts
@@ -1,9 +1,29 @@
-import pyatv from '@sebbo2002/node-pyatv';
+import { NodePyATVDevice, NodePyATVFindAndInstanceOptions, NodePyATVInstance } from '@sebbo2002/node-pyatv';
 
 
 const atvremote_path = `${__dirname}/.venv/bin/atvremote`;
 const atvscript_path = `${__dirname}/.venv/bin/atvscript`;
 
-const pyatvInstance = new pyatv({debug: true, atvscriptPath: atvscript_path, atvremotePath: atvremote_path});
+interface ICachedDevice {
+    id: string;
+    host: string;
+    name: string;
+}
+
+class CustomPyAtvInstance extends NodePyATVInstance {
+
+    cachedDevices: NodePyATVDevice[] = [];
+
+    async find(options?: NodePyATVFindAndInstanceOptions | undefined): Promise<NodePyATVDevice[]> {
+        this.cachedDevices = await super.find(options);
+        return this.cachedDevices;
+    }
+
+    deviceById(id: string): NodePyATVDevice | undefined {
+        return this.cachedDevices.find((d) => d.id === id);
+    }
+}
+
+const pyatvInstance = new CustomPyAtvInstance({ debug: true, atvscriptPath: atvscript_path, atvremotePath: atvremote_path });
 
 export default pyatvInstance;
